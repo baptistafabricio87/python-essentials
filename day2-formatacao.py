960448ec15081ed34339d381e03633f1a2c3b90d
Fabricio Castro <baptistafabricio87@gmail.com>
2023-08-30 14:25:10 -0300

    exercicio de formatação de texto

---
+++ day2-formatacao.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env/ python3
+# coding: utf-8
+# interpolação
+
+__author__ = 'Fabricio'
+
+template = 'O saldo de %s é o total de %.2f'
+nome = 'Fabricio'
+saldo = 30.0
+print(template % (nome, saldo))
