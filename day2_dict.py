7762e511e8072af9fbe5523becc001e8f25d4ef2
Fabricio Castro <baptistafabricio87@gmail.com>
2023-09-02 14:59:34 -0300

    Exemplos de aula sobre dicionarios.

---
+++ day2_dict.py
@@ -0,0 +1,24 @@
+# coding: utf-8
+{'nome': 'Fabricio', 'cod': 123}
+cliente = {'nome': 'Fabricio', 'cod': 123}
+cliente['nome']
+cliente['cod']
+dir(cliente)
+cliente['cidade'] = 'Santos
+cliente['cidade'] = 'Santos'
+len(cliente)
+del cliente['cidade']
+cliente
+'Fabricio' in cliente
+1nome' in cliente
+'nome' in cliente
+cliente['nome'] == 'Fabricio'
+cliente['nome'] = 'Fabricio Castro'
+clinete
+cliente
+extra = {'cidade': 'Santos'}
+cliente
+extra
+final = {**cliente, **extra}
+final
+# ** sinaliza o desempacotamento do objeto, retirando as informacoes do objeto extra e unindo ao objeto cliente, gerando um novo dicionario 'final'
